10be588d923099d2a7f0edcec5b9e1f128af64ec
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-02-08 01:54:26 +0530

    creating product database

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const mongoConnetct = require("../util/database");
+const getDb = require("../util/database").getDb;
 class Product {
   constructor(title, price, description, imageUrl) {
     this.title = title;
@@ -6,29 +7,18 @@ class Product {
     this.description = description;
     this.imageUrl = imageUrl;
   }
-  save() {}
+  save() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
-
-// const Product = sequelize.define("product", {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true,
-//   },
-//   title: Sequelize.STRING,
-//   price: {
-//     type: Sequelize.DOUBLE,
-//     allowNull: false,
-//   },
-//   imageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   },
-//   description: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   },
-// });
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -2,17 +2,28 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 require("dotenv").config();
 
+let db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.utikq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@learning.utikq.mongodb.net/?retryWrites=true&w=majority&appName=learning`
   )
-    .then((result) => {
+    .then((client) => {
       console.log("connected to MongoDB");
-      callback(result);
+      db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (db) {
+    return db;
+  }
+  throw "no database found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
