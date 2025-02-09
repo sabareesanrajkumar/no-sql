99ef2f4d07b90a977ef9825cadfc03eaba0b63d6
sabareesanrajkumar <sabareesanrajkumar05@gmail.com>
2025-02-09 12:45:12 +0530

    deleting cart items

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = IdleDeadline;
   }
 
   save() {
@@ -19,6 +21,30 @@ class User {
       });
   }
 
+  addToCart(product) {
+    const cartProduct = this.cart.items.findIndex((cp) => {
+      return cp._id === product._id;
+    });
+    product.quantity = 1;
+    const updatedCart = { items: [product] };
+    const db = getDb();
+    db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId !== productId.toString();
+    });
+    const db = getDb();
+    db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: { items: updatedCartItems } } }
+    );
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
@@ -26,6 +52,7 @@ class User {
       .findOne({ _id: new mongodb.ObjectId(userId) })
       .then((user) => {
         console.log(user);
+        return user;
       })
       .catch((err) => {
         console.log(err);
